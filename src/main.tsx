d0cfd16d456a54c9533b6de69965528c441e520f
Jinxi Yu <jasminelyvia07@gmail.com>
2025-11-28 14:29:13 -0500

    fix main back without electron

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
@@ -9,8 +9,8 @@ import Home from './pages/Home'
 
 import Callback from './functions/Callback'
 
-const queryclient  = new QueryClient();
 
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -33,8 +33,11 @@ const rootEle = document.getElementById('root');
 if(rootEle){
   createRoot(rootEle).render(
   <StrictMode>
-    <QueryClientProvider client={queryclient}>
-       <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}>
+      
+      </RouterProvider>
+      
     </QueryClientProvider>
   
   </StrictMode>,
